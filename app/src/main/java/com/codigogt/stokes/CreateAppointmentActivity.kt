7d3aa00c9ab58ed643c93e1a1748eec63dedda32
newtella <fl.mlopezgt@gmail.com>
2019-10-30 16:34:42 -0600

    new appointment views

--- app/src/main/java/com/codigogt/stokes/CreateAppointmentActivity.kt
+++ app/src/main/java/com/codigogt/stokes/CreateAppointmentActivity.kt
@@ -2,6 +2,10 @@ package com.codigogt.stokes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_create_appointment.*
 
 
 class CreateAppointmentActivity : AppCompatActivity() {
@@ -10,6 +14,20 @@ class CreateAppointmentActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_appointment)
 
+        btnNext.setOnClickListener {
+            cvStep1.visibility = View.GONE
+            cvStep2.visibility = View.VISIBLE
+        }
 
+        btnConfirm.setOnClickListener {
+            Toast.makeText(this, "Cita creada correctamente", Toast.LENGTH_SHORT).show()
+            this.finish()
+        }
+
+        val specialtyOptions = arrayOf("Neurologia", "Cardiologia", "Medico y Cirujano")
+        spinnerSpecialties.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, specialtyOptions)
+
+        val doctorOptions = arrayOf("Henry Diaz", "Tomas Ramos", "Dorian Velasquez")
+        spinnerDoctors.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, doctorOptions)
     }
 }
